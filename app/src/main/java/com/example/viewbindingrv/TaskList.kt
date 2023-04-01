2331aa12c12c2078d56ae36b571a44df139011d2
Asmit2021 <2140149@sliet.ac.in>
2023-04-01 07:45:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,16 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a Walk", timeStamp = "7:00am"),
+        Task("Eat BreakFast", timeStamp = "8:00am"),
+        Task("Attend a Meeting","9:00am"),
+        Task("Read a Book","12:00pm"),
+        Task("Eat Lunch", "2:00pm"),
+        Task("Buy Groceries","4:00pm"),
+        Task("Watch Youtube Videos","6:00pm"),
+        Task("Eat Dinner","7:00pm"),
+        Task("Go to Bed","9:00pm"),
+        Task("Midnight Snack","00:00am")
+    )
+}
